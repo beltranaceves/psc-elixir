39dd98fa8989157c278c9fea21c4de39dcbbb734
beltranaceves <beltranaceves@gmail.com>
2026-04-08 11:39:55 +0200

    [ADD]: unstructure ed canvas model attempt

--- mix.exs
+++ mix.exs
@@ -68,7 +68,8 @@ defmodule Psc.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:mishka_chelekom, "~> 0.0.8", only: :dev}
+      {:mishka_chelekom, "~> 0.0.8", only: :dev},
+      {:justify, "~> 1.2"}
     ]
   end
 
